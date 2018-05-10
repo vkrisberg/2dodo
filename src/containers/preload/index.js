4692330b55dd94e6c72adcb8f69f805e53b040d7
darotar <fabledva@mail.ru>
2018-05-10 15:59:29 +0400

    Preload Screen ready

--- src/containers/preload/index.js
+++ src/containers/preload/index.js
@@ -3,7 +3,10 @@ import { View, Image, Text } from 'react-native';
 
 import Title from '../../components/elements/title';
 import routeEnum from '../../enums/route-enum';
-import chatIcon from '../login/img/chat.png';
+import logoIcon from './img/logo.png';
+import backgroundImage from './img/background.jpg';
+import logoTitleIcon from './img/title.png';
+import wavesImage from './img/waves.png';
 
 import styles from './styles';
 
@@ -19,11 +22,15 @@ export default class Preload extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Image style={styles.image} source={chatIcon} />
-        <Title>2DODO</Title>
+        <Image style={styles.background} source={backgroundImage} />
+        <View style={styles.logoContainer}>
+          <Image style={styles.logo} source={logoIcon} />
+          <Image style={styles.logoTitle} source={logoTitleIcon} />
+        </View>
         <Text style={styles.text}>
           Do what you want
         </Text>
+        <Image style={styles.waves} source={wavesImage} />
       </View>
     );
   }
--- src/containers/preload/styles.js
+++ src/containers/preload/styles.js
@@ -7,17 +7,40 @@ export default StyleSheet.create({
     alignItems: 'center'
   },
 
-  image: {
-    width: 100,
-    height: 100,
-    alignSelf: 'center',
-    marginTop: 50,
-    marginBottom: 50
+  background: {
+    position: 'absolute',
+    resizeMode: 'cover'
+  },
+
+  logo: {
+    width: 80,
+    height: 80,
+    marginBottom: 20
+  },
+
+  logoContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+
+  logoTitle: {
+    width: 122,
+    height: 31
   },
 
   text: {
     alignSelf: 'center',
     position: 'absolute',
-    bottom: 50
+    bottom: 75,
+    fontSize: 15,
+    color: 'white'
+  },
+
+  waves: {
+    position: 'absolute',
+    height: 35,
+    resizeMode: 'center',
+    bottom: 0
   }
 });
