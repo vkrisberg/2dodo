7d4620bdf7ef39d409c258081f2f2f0b16c46bb5
Ramil Zaynetdinov <ramil.zv@gmail.com>
2018-06-08 20:17:55 +0300

    #I-4329 #comment added netInfo

--- src/styles.js
+++ src/styles.js
@@ -1,3 +1,13 @@
+import {Dimensions, Platform} from 'react-native';
+
+const OS = Platform.OS;
+Dimensions.APPBAR_HEIGHT = OS === 'ios' ? 44 : 56;
+Dimensions.NAVBAR_HEIGHT = OS === 'ios' ? 64 : 56;
+Dimensions.STATUSBAR_HEIGHT = OS === 'ios' ? 20 : 0;
+Dimensions.IS_IPHONE_5 = Dimensions.get('window').width < 375;
+Dimensions.IS_IPHONE_6 = Dimensions.get('window').width === 375;
+Dimensions.IS_IPHONE_PLUS = Dimensions.get('window').width > 375;
+
 const colors = {
   white: '#ffffff',
   black: '#000000',
@@ -8,7 +18,19 @@ const fonts = {
   main: 'Exo 2',
 };
 
+const sizes = {
+  appbarHeight: Dimensions.APPBAR_HEIGHT,
+  navbarHeight: Dimensions.NAVBAR_HEIGHT,
+  statusbarHeight: Dimensions.STATUSBAR_HEIGHT,
+  isIphone5: Dimensions.IS_IPHONE_5,
+  isIphone6: Dimensions.IS_IPHONE_6,
+  isIphonePlus: Dimensions.IS_IPHONE_PLUS,
+  windowWidth: Dimensions.get('window').width,
+  windowHeight: Dimensions.get('window').height,
+};
+
 export {
   colors,
   fonts,
+  sizes,
 }
