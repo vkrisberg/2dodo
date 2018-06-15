fa3d4202afe6d6ff833a33445aec93e32a4e0e19
sveta <svetlana.shurla4acova@gmail.com>
2018-06-15 15:41:38 +0300

    #I-4475 - changed ChatItem component

--- src/components/elements/chat-item/styles.js
+++ src/components/elements/chat-item/styles.js
@@ -1,59 +1,97 @@
 import styled from 'styled-components';
-
-export const Chat = styled.View`
-  width: 92%;
-  height: 56px;
-  margin-vertical: 8px;
-  margin-horizontal: auto;
-  align-items: center;
-  flex-direction: row;
-`;
+import {colors, fonts, weights} from "../../../styles";
+import {StyleSheet} from "react-native";
 
 export const ChatChosen = {
   marginBottom: -6
 };
 
-export const ChatImage = styled.View`
-  width: 56px;
-  height: 56px;
-  background-color: #e4e4e4;
-  border-radius: 50;
-  margin-left: 8px;
-  align-items: center;
-  justify-content: center;
-`;
+export default (theme) => {
+  return StyleSheet.create({
+    avatarContainer: {
+      width: 340,
+      height: 50,
+      marginVertical: 8,
+      marginHorizontal: 'auto',
+      flexDirection: 'row',
+    },
 
-export const ChatInformation = styled.View`
-  height: 56px;
-  margin-left: 16px;
-`;
+    image: {
+      width: 50,
+      height: 50,
+      flexShrink: 0,
+      borderRadius: 50,
+      marginLeft: 10,
+      alignItems: 'center',
+      justifyContent: 'center',
+      backgroundColor: colors[theme].blue,
+      overflow: 'hidden',
+    },
 
-export const ChatBody = ChatInformation.extend`
-  width: 195;
-`;
+    avatar: {
+      minWidth: '100%',
+      minHeight: '100%',
+    },
 
-export const ChatName = styled.Text`
-  font-weight: bold;
-  align-self: flex-start;
-  font-size: 16px;
-`;
+    avatarInitials: {
+      color: colors[theme].white,
+      fontFamily: fonts.main,
+      fontSize: 16,
+      fontWeight: weights.semiBold,
+    },
 
-export const ChatMessage = styled.Text`
-`;
+    body: {
+      marginLeft: 15,
+      flexGrow: 0,
+      width: 205,
+    },
 
-export const ChatMessageDate = styled.Text`
-  font-size: 15px;
-`;
+    name: {
+      color: colors[theme].grayBlue,
+      fontFamily: fonts.main,
+      fontSize: 16,
+      fontWeight: weights.semiBold,
+    },
 
-export const ChatNotReadenMessage = styled.View`
-  align-items: center;
-  justify-content: center;
-  background-color: #5395f3;
-  border-radius: 50;
-  width: 36px;
-  margin-top: 8px;
-`;
+    text: {
+      color: colors[theme].grayText,
+      fontFamily: fonts.main,
+      fontSize: 13,
+      fontWeight: weights.medium,
+    },
 
-export const ChatNotReadenMessageText = styled.Text`
-  color: white;
-`;
+    notReadenMessage: {
+      height: 20,
+      paddingHorizontal: 10,
+      alignItems: 'center',
+      justifyContent: 'center',
+      borderRadius: 50,
+      marginTop: 8,
+      backgroundColor: colors[theme].blue,
+    },
+
+    notReadenMessageText: {
+      color: colors[theme].white,
+      fontFamily: fonts.main,
+      fontSize: 13,
+      fontWeight: weights.medium,
+    },
+
+    information: {
+      height: '100%',
+      width: 50,
+      flexShrink: 0,
+      alignItems: 'flex-end',
+      marginLeft: 5,
+    },
+
+    limitText: {
+      color: colors[theme].grayText,
+      fontFamily: fonts.main,
+      fontSize: 13,
+      fontWeight: weights.medium,
+      height: 30,
+      overflow: 'hidden',
+    },
+  });
+};
--- src/containers/messages/main/index.js
+++ src/containers/messages/main/index.js
@@ -17,6 +17,101 @@ import {
   StyledIcon
 } from '../styles';
 
+const list = [
+  {
+    id: '', // unique chat id (uuid4)
+    name: 'Gomer Simpson',
+    owner: '',
+    members: [],
+    shortName: '',
+    avatar: '',
+    lastMessage: {
+      id: 'id',
+      chatId: 'id2',
+      type: 'text', // [text, audio, video, image, call]
+      username: 'Kolya',
+      from: 'Gomer Simpson',
+      text: 'Hello!',
+      fileUrl: '',
+      user: {
+        name: 'User',
+        primaryKey: 'username',
+        properties: {
+          username: 'string', // login@hostname
+          nickname: 'string', // login
+          email: 'string',
+          phones: 'string?[]',
+          firstName: 'string?',
+          secondName: 'string?',
+          bio: 'string?',
+          avatar: 'https://st.kp.yandex.net/images/actor_iphone/iphone360_110.jpg',
+          theme: 'string',
+        },
+      },
+      quote: {},
+      status: 'sending', // [sending, send, received, read, error]
+      isOwn: false,
+      isFavorite: false,
+      salt: '',
+      dateSend: null,
+      dateCreate: null,
+      dateUpdate: null,},
+    unreadCount: 1,
+    sort: 0,
+    pin: 0,
+    isMuted: false,
+    isDeleted: false,
+    dateCreate: null,
+    dateUpdate: '2018-06-15 10:00',
+  },
+  {
+    id: '', // unique chat id (uuid4)
+    name: 'Lisa Simpson',
+    owner: '',
+    members: [],
+    shortName: '',
+    avatar: 'http://i.imgur.com/4LClmI1.png',
+    lastMessage: {
+      id: 'id',
+      chatId: 'id2',
+      type: 'text', // [text, audio, video, image, call]
+      username: 'Kolya',
+      from: 'Lisa Simpson',
+      text: 'Lisa how many times do you talk your music is dangerous to the ears!',
+      fileUrl: '',
+      user: {
+        name: 'User',
+        primaryKey: 'username',
+        properties: {
+          username: 'string', // login@hostname
+          nickname: 'string', // login
+          email: 'string',
+          phones: 'string?[]',
+          firstName: 'string?',
+          secondName: 'string?',
+          bio: 'string?',
+          avatar: 'https://st.kp.yandex.net/images/actor_iphone/iphone360_110.jpg',
+          theme: 'string',
+        },
+      },
+      quote: {},
+      status: 'sending', // [sending, send, received, read, error]
+      isOwn: false,
+      isFavorite: false,
+      salt: '',
+      dateSend: null,
+      dateCreate: null,
+      dateUpdate: null,},
+    unreadCount: 0,
+    sort: 0,
+    pin: 0,
+    isMuted: false,
+    isDeleted: false,
+    dateCreate: null,
+    dateUpdate: '2018-06-14 10:00',
+  },
+];
+
 class Messages extends PureComponent {
 
   static propTypes = {
@@ -137,7 +232,7 @@ class Messages extends PureComponent {
           </TouchableWithoutFeedback>
         </Header>
         <SearchInput placeholder="Search in chats" onChange={this.searchChats}/>
-        <ChatsBody chatList={chat.list} onChatPress={this.onChatPress}/>
+        <ChatsBody context={this.context} chatList={list} onChatPress={this.onChatPress}/>
       </Wrapper>
     );
   }
