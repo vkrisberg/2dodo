8226c984ca19bf2e144df8902851234b7b14443e
Ramil Zaynetdinov <ramil.zv@gmail.com>
2018-06-21 00:04:03 +0300

    #I-4228 #comment added group websocket and redux actions

--- src/api/group.js
+++ src/api/group.js
@@ -26,4 +26,119 @@ export default {
 
     return serverMessage;
   },
+
+  deleteGroup: async (link) => {
+    const websocket = services.getWebsocket();
+    const serverMessage = await wsMessage.getServerMessage({
+      action: actionEnum.deleteGroup,
+      data: {
+        link,
+      },
+    });
+
+    websocket.send(JSON.stringify(serverMessage.message));
+
+    return serverMessage;
+  },
+
+  getGroup: async (link) => {
+    const websocket = services.getWebsocket();
+    const serverMessage = await wsMessage.getServerMessage({
+      action: actionEnum.getGroup,
+      data: {
+        link,
+      },
+    });
+
+    websocket.send(JSON.stringify(serverMessage.message));
+
+    return serverMessage;
+  },
+
+  getPublicGroupList: async () => {
+    const websocket = services.getWebsocket();
+    const serverMessage = await wsMessage.getServerMessage({
+      action: actionEnum.getPublicGroupList,
+    });
+
+    websocket.send(JSON.stringify(serverMessage.message));
+
+    return serverMessage;
+  },
+
+  inviteMembers: async (data) => {
+    const websocket = services.getWebsocket();
+    const serverMessage = await wsMessage.getServerMessage({
+      action: actionEnum.inviteMembersToGroup,
+      data,
+    });
+
+    websocket.send(JSON.stringify(serverMessage.message));
+
+    return serverMessage;
+  },
+
+  subscribeToGroup: async (link) => {
+    const websocket = services.getWebsocket();
+    const serverMessage = await wsMessage.getServerMessage({
+      action: actionEnum.subscribeToGroup,
+      data: {
+        link,
+      },
+    });
+
+    websocket.send(JSON.stringify(serverMessage.message));
+
+    return serverMessage;
+  },
+
+  unsubscribeFromGroup: async (link) => {
+    const websocket = services.getWebsocket();
+    const serverMessage = await wsMessage.getServerMessage({
+      action: actionEnum.unsubscribeFromGroup,
+      data: {
+        link,
+      },
+    });
+
+    websocket.send(JSON.stringify(serverMessage.message));
+
+    return serverMessage;
+  },
+
+  getGroupMember: async (data) => {
+    const websocket = services.getWebsocket();
+    const serverMessage = await wsMessage.getServerMessage({
+      action: actionEnum.getGroupMember,
+      data,
+    });
+
+    websocket.send(JSON.stringify(serverMessage.message));
+
+    return serverMessage;
+  },
+
+  updateGroupMember: async (data) => {
+    const websocket = services.getWebsocket();
+    const serverMessage = await wsMessage.getServerMessage({
+      action: actionEnum.updateGroupMember,
+      data,
+    });
+
+    websocket.send(JSON.stringify(serverMessage.message));
+
+    return serverMessage;
+  },
+
+  sendGroupMessage: async (data) => {
+    const websocket = services.getWebsocket();
+    const serverMessage = await wsMessage.getServerMessage({
+      action: actionEnum.sendGroupMessage,
+      data,
+    });
+
+    websocket.send(JSON.stringify(serverMessage.message));
+
+    return serverMessage;
+  },
 };
--- src/store/group/actions.js
+++ src/store/group/actions.js
@@ -26,10 +26,38 @@ export const types = {
   DELETE_SUCCESS: Symbol('DELETE_SUCCESS'),
   DELETE_FAILURE: Symbol('DELETE_FAILURE'),
 
-  RECEIVE_CHAT_SUCCESS: Symbol('RECEIVE_CHAT_SUCCESS'),
-  RECEIVE_CHAT_FAILURE: Symbol('RECEIVE_CHAT_FAILURE'),
+  GET_GROUP: Symbol('GET_GROUP'),
+  GET_GROUP_SUCCESS: Symbol('GET_GROUP_SUCCESS'),
+  GET_GROUP_FAILURE: Symbol('GET_GROUP_FAILURE'),
 
-  SET_CURRENT_CHAT: Symbol('SET_CURRENT_CHAT'),
+  GET_PUBLIC_LIST: Symbol('GET_PUBLIC_LIST'),
+  GET_PUBLIC_LIST_SUCCESS: Symbol('GET_PUBLIC_LIST_SUCCESS'),
+  GET_PUBLIC_LIST_FAILURE: Symbol('GET_PUBLIC_LIST_FAILURE'),
+
+  INVITE: Symbol('INVITE'),
+  INVITE_SUCCESS: Symbol('INVITE_SUCCESS'),
+  INVITE_FAILURE: Symbol('INVITE_FAILURE'),
+
+  SUBSCRIBE: Symbol('SUBSCRIBE'),
+  SUBSCRIBE_SUCCESS: Symbol('SUBSCRIBE_SUCCESS'),
+  SUBSCRIBE_FAILURE: Symbol('SUBSCRIBE_FAILURE'),
+
+  UNSUBSCRIBE: Symbol('UNSUBSCRIBE'),
+  UNSUBSCRIBE_SUCCESS: Symbol('UNSUBSCRIBE_SUCCESS'),
+  UNSUBSCRIBE_FAILURE: Symbol('UNSUBSCRIBE_FAILURE'),
+
+  GET_MEMBER: Symbol('GET_MEMBER'),
+  GET_MEMBER_SUCCESS: Symbol('GET_MEMBER_SUCCESS'),
+  GET_MEMBER_FAILURE: Symbol('GET_MEMBER_FAILURE'),
+
+  UPDATE_MEMBER: Symbol('UPDATE_MEMBER'),
+  UPDATE_MEMBER_SUCCESS: Symbol('UPDATE_MEMBER_SUCCESS'),
+  UPDATE_MEMBER_FAILURE: Symbol('UPDATE_MEMBER_FAILURE'),
+
+  RECEIVE_INVITE_SUCCESS: Symbol('RECEIVE_INVITE_SUCCESS'),
+  RECEIVE_INVITE_FAILURE: Symbol('RECEIVE_INVITE_FAILURE'),
+
+  SET_CURRENT_GROUP: Symbol('SET_CURRENT_GROUP'),
 };
 
 export default {
@@ -69,6 +97,9 @@ export default {
       try {
         const realm = services.getRealm();
         const group = realm.objectForPrimaryKey(dbEnum.Group, id);
+        if (!group) {
+          throw new Error('group is not found in database');
+        }
         // console.log('group loaded', group);
         const payload = {...group};
         dispatch({type: types.LOAD_ONE_SUCCESS, payload});
@@ -86,6 +117,20 @@ export default {
       try {
         const realm = services.getRealm();
         const {account} = getState();
+
+        data = omitBy(data, isUndefined);
+        if (!data.link) {
+          throw new Error('create group failed: link is empty');
+        }
+
+        if (!data.type) {
+          throw new Error('create group failed: group type is empty');
+        }
+
+        if (!data.name) {
+          throw new Error('create group failed: name is empty');
+        }
+
         const dateNow = new Date();
         const sendData = {
           link: data.link,
@@ -125,14 +170,24 @@ export default {
       dispatch({type: types.UPDATE});
       try {
         const realm = services.getRealm();
+
         data = omitBy(data, isUndefined);
+        if (!data.link) {
+          throw new Error('update group failed: link is empty');
+        }
+
+        if (!data.name) {
+          throw new Error('update group failed: name is empty');
+        }
+
+        data.dateUpdate = new Date();
         const sendData = {
           link: data.link,
           name: data.name,
           description: data.description || '',
           avatar: data.avatar || '',
         };
-        data.dateUpdate = new Date();
+
         let group = {};
         await realm.write(() => {
           group = realm.create(dbEnum.Group, data, true);
@@ -161,16 +216,21 @@ export default {
           throw new Error('delete failed: groups are not found');
         }
 
+        const deletedGroups = groups.map((item) => {
+          return {...item};
+        });
+
         await realm.write(() => {
           realm.delete(groups);
         });
 
-        for (let i = 0; i < ids.length; i++) {
-          const groupId = ids[i];
-          const messages = realm.objects(dbEnum.GroupMessage).filtered(`groupId = '${groupId}'`);
+        for (let i = 0; i < deletedGroups.length; i++) {
+          const group = deletedGroups[i];
+          const messages = realm.objects(dbEnum.GroupMessage).filtered(`groupId = '${group.id}'`);
           await realm.write(() => {
             realm.delete(messages);
           });
+          await apiGroup.deleteGroup(group.link);
         }
 
         // console.log('groups deleted', ids);
@@ -194,6 +254,8 @@ export default {
           throw new Error('delete failed: group is not found');
         }
 
+        const deletedGroup = {...group};
+
         await realm.write(() => {
           realm.delete(group);
         });
@@ -202,6 +264,7 @@ export default {
         await realm.write(() => {
           realm.delete(messages);
         });
+        await apiGroup.deleteGroup(deletedGroup.link);
         // console.log('group deleted', id);
         dispatch({type: types.DELETE_SUCCESS, payload: id});
         return true;
@@ -212,58 +275,79 @@ export default {
     };
   },
 
-  receiveChat: (message) => {
-    return async (dispatch, getState) => {
+  getGroup: (link) => {
+    return async dispatch => {
+      dispatch({type: types.GET_GROUP, payload: link});
       try {
-        const realm = services.getRealm();
-        const {account} = getState();
-        const dateNow = new Date();
-        const dataPayload = get(message, 'data.payload', {});
-        if (!dataPayload) {
-          throw new Error('data.payload is null');
+        if (!link) {
+          throw new Error('get group failed: link is empty');
         }
-        const decryptedData = await wsMessage.decryptClientMessage({
-          data: dataPayload,
-          privateKey: account.keys.privateKey
-        });
-        const members = filter(decryptedData.members, (username) => username !== account.user.username);
-        const contacts = map(members, (username) => {
-          return {
-            username: username,
-            nickname: username.split('@')[0],
-          };
-        });
-        const chatData = {
-          ...decryptedData,
-          name: map(contacts, 'nickname').join(', '),
-          shortName: wsMessage.getShortName(contacts),
-          isShown: false,
-          dateSend: wsMessage.dateSendToDate(decryptedData.dateSend),
-          dateCreate: dateNow,
-          dateUpdate: dateNow,
-        };
-        let chat = {};
-        await realm.write(() => {
-          chat = realm.create(dbEnum.Chat, chatData, true);
-        });
-        const payload = {...chat};
-        // console.log('chat received', chat);
-        const hashKeyData = {
-          chatId: chat.id,
-          hashKey: wsMessage.hashFromMessage(decryptedData),
-          dateSend: chat.dateSend,
-        };
-        await hashKeyAdd(hashKeyData);
-        dispatch({type: types.RECEIVE_CHAT_SUCCESS, payload});
-        return payload;
+        return await apiGroup.getGroup(link);
       } catch (e) {
-        dispatch({type: types.RECEIVE_CHAT_FAILURE, error: e});
+        dispatch({type: types.GET_GROUP_FAILURE, error: e});
         throw e;
       }
     };
   },
 
-  setCurrentChat: (data) => {
-    return {type: types.SET_CURRENT_CHAT, payload: data};
+  getPublicGroupList: () => {
+    return async dispatch => {
+      dispatch({type: types.GET_PUBLIC_LIST});
+      try {
+        return await apiGroup.getPublicGroupList();
+      } catch (e) {
+        dispatch({type: types.GET_PUBLIC_LIST_FAILURE, error: e});
+        throw e;
+      }
+    };
+  },
+
+  inviteMembers: ({link, members}) => {
+    return async dispatch => {
+      dispatch({type: types.INVITE, payload: {link, members}});
+      try {
+        if (!link || !members || !members.length) {
+          throw new Error('invite members failed: link or members empty');
+        }
+        return await apiGroup.inviteMembers({link, members});
+      } catch (e) {
+        dispatch({type: types.INVITE_FAILURE, error: e});
+        throw e;
+      }
+    };
+  },
+
+  subscribeToGroup: (link) => {
+    return async dispatch => {
+      dispatch({type: types.SUBSCRIBE, payload: {link, members}});
+      try {
+        if (!link) {
+          throw new Error('subscribe to group failed: link is empty');
+        }
+        return await apiGroup.subscribeToGroup(link);
+      } catch (e) {
+        dispatch({type: types.SUBSCRIBE_FAILURE, error: e});
+        throw e;
+      }
+    };
+  },
+
+  unsubscribeFromGroup: (link) => {
+    return async dispatch => {
+      dispatch({type: types.UNSUBSCRIBE, payload: {link, members}});
+      try {
+        if (!link) {
+          throw new Error('unsubscribe from group failed: link is empty');
+        }
+        return await apiGroup.unsubscribeFromGroup(link);
+      } catch (e) {
+        dispatch({type: types.UNSUBSCRIBE_FAILURE, error: e});
+        throw e;
+      }
+    };
+  },
+
+  setCurrentGroup: (data) => {
+    return {type: types.SET_CURRENT_GROUP, payload: data};
   },
 };
